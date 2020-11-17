bca6a663512a79bd543eb0dccd5aef6a68623211
iannisdezwart <iannis__@hotmail.com>
2020-11-17 11:13:35 +0100

    🚀 First Commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var data;
+var generateCountryCSV = function (countryName) {
+    var csvData = 'date;confirmed;deaths;recovered\n';
+    for (var _i = 0, _a = data[countryName]; _i < _a.length; _i++) {
+        var day = _a[_i];
+        var date = new Date(day.date);
+        var formattedDate = date.getFullYear() + "-" + (date.getMonth() + 1).toString().padStart(2, '0') + "-" + (date.getDate() + 1).toString().padStart(2, '0');
+        csvData += formattedDate + ";" + day.confirmed + ";" + day.deaths + ";" + day.recovered + "\n";
+    }
+    var file = new File([csvData], countryName + '.csv', { type: 'text/csv' });
+    var downloadButton = document.createElement('a');
+    downloadButton.href = URL.createObjectURL(file);
+    downloadButton.download = countryName + '.csv';
+    document.body.appendChild(downloadButton);
+    downloadButton.click();
+    downloadButton.remove();
+};
+fetch('https://pomber.github.io/covid19/timeseries.json')
+    .then(function (res) { return res.json(); })
+    .then(function (json) {
+    data = json;
+    document.body.innerHTML = /* html */ "\n\t\t<button onclick=\"generateCountryCSV('Netherlands')\">Netherlands</button>\n\t\t<button onclick=\"generateCountryCSV('Australia')\">Australia</button>\n\t\t<button onclick=\"generateCountryCSV('Brazil')\">Brazil</button>\n\t\t<button onclick=\"generateCountryCSV('India')\">India</button>\n\t\t<button onclick=\"generateCountryCSV('Afghanistan')\">Afghanistan</button>\n\t\t<button onclick=\"generateCountryCSV('Russia')\">Russia</button>\n\t\t<button onclick=\"generateCountryCSV('Germany')\">Germany</button>\n\t\t";
+});
---
+++ index.ts
@@ -0,0 +1,48 @@
+interface Data {
+	[keys: string]: {
+		date: string,
+		confirmed: number,
+		deaths: number,
+		recovered: number
+	}[]
+}
+
+let data: Data
+
+const generateCountryCSV = (
+	countryName: string
+) => {
+	let csvData = 'date;confirmed;deaths;recovered\n'
+
+	for (let day of data[countryName]) {
+		const date = new Date(day.date)
+		const formattedDate = `${ date.getFullYear() }-${ (date.getMonth() + 1).toString().padStart(2, '0') }-${ (date.getDate() + 1).toString().padStart(2, '0') }`
+
+		csvData += `${ formattedDate };${ day.confirmed };${ day.deaths };${ day.recovered }\n`
+	}
+
+	const file = new File([ csvData ], countryName + '.csv', { type: 'text/csv' })
+
+	const downloadButton = document.createElement('a')
+	downloadButton.href = URL.createObjectURL(file)
+	downloadButton.download = countryName + '.csv'
+	document.body.appendChild(downloadButton)
+	downloadButton.click()
+	downloadButton.remove()
+}
+
+fetch('https://pomber.github.io/covid19/timeseries.json')
+	.then(res => res.json())
+	.then(json => {
+		data = json as Data
+
+		document.body.innerHTML = /* html */ `
+		<button onclick="generateCountryCSV('Netherlands')">Netherlands</button>
+		<button onclick="generateCountryCSV('Australia')">Australia</button>
+		<button onclick="generateCountryCSV('Brazil')">Brazil</button>
+		<button onclick="generateCountryCSV('India')">India</button>
+		<button onclick="generateCountryCSV('Afghanistan')">Afghanistan</button>
+		<button onclick="generateCountryCSV('Russia')">Russia</button>
+		<button onclick="generateCountryCSV('Germany')">Germany</button>
+		`
+	})
